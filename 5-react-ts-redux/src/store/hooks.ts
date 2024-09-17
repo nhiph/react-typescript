ef73f1a586e5c0ebc79c1fbf0c6e40871e0d8cdd
nhiph <phunghuynhnhi797@gmail.com>
2024-09-17 11:23:44 +0700

    5-react-ts-redux

---
+++ 5-react-ts-redux/src/store/hooks.ts
@@ -0,0 +1,10 @@
+/** @format */
+
+import { useDispatch, useSelector, TypedUseSelectorHook } from "react-redux";
+import { type AppDispatch, RootState } from "./store.ts";
+
+type DispatchFunction = () => AppDispatch;
+
+export const useCartDispatch: DispatchFunction = useDispatch;
+
+export const useCartSelector: TypedUseSelectorHook<RootState> = useSelector;
